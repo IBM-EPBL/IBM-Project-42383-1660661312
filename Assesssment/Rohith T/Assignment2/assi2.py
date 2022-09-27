34412716ec863f2a06bb7bcc76813fe87dd2f120
THATIPARTHIROHITH <113966377+THATIPARTHIROHITH@users.noreply.github.com>
2022-09-27 13:50:16 +0530

    Add files via upload

---
+++ Assesssment/Rohith T/Assignment2/assi2.py
@@ -0,0 +1,9 @@
+import random, time, winsound
+while True:
+    temp = random.randint(1, 100)
+    if temp > 60:
+        print("Alert Temperature is high!!")
+        winsound.PlaySound("beep-01a.wav", winsound.SND_FILENAME)
+    else:
+        print("Temperature is Normal")
+    time.sleep(1)
